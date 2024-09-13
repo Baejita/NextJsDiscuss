a42ac30e21d462da3a0954608b1e78445ed9f2cc
Baejita <homokoro@gmail.com>
2024-09-13 13:00:33 +0700

    OKerror useFormState

--- app/components/topics/topicCreateForm.tsx
+++ app/components/topics/topicCreateForm.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import * as actions from "@/app/actions";
 import {
   Input,
@@ -7,15 +9,19 @@ import {
   PopoverTrigger,
   PopoverContent,
 } from "@nextui-org/react";
+import { useFormState } from "react-dom";
 
 function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors:{}
+  })
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Toppic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className=" flex flex-col gap-4 p-4 w-80">
             <h3 className="text-xl">Create a Topic</h3>
             <Input
@@ -23,12 +29,17 @@ function TopicCreateForm() {
               label="Name"
               labelPlacement="outside"
               placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')}
             />
+          
             <Textarea
               name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe yout topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
             />
 
             <Button type="submit">Submit</Button>
